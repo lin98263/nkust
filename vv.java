9f40ca9be4d47247274c2dfab83a74fc25cf9ace
lin98263 <93572449+lin98263@users.noreply.github.com>
2021-11-16 16:44:45 +0800

    Add files via upload

---
+++ vv.java
@@ -0,0 +1,16 @@
+public class vv {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            boolean k = true;
+            for (int n = 2; n < i; n++) {
+                if (i % n == 0) {
+                    k = false;
+                    break;
+                }
+            }
+            if (k) {
+                System.out.println(i);
+            }
+        }
+    }
+}
