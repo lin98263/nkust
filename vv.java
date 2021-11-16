ee2302f5014525bd508925dc8244ff31bb8bd533
lin98263 <93572449+lin98263@users.noreply.github.com>
2021-11-16 16:51:28 +0800

    Update vv.java

--- vv.java
+++ vv.java
@@ -1,15 +1,14 @@
 public class vv {
     public static void main(String[] args) {
         for (int i = 2; i <= 100; i++) {
-            boolean k = true;
             for (int n = 2; n < i; n++) {
                 if (i % n == 0) {
-                    k = false;
+                    break;
+                } else if (n == i - 1) {
+                    System.out.println(i);
                     break;
                 }
-            }
-            if (k) {
-                System.out.println(i);
+
             }
         }
     }
